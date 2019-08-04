37064802df05dc6c82ad4d1fe25f183338254e64
Andre Pato <anpato1994@gmail.com>
2019-08-04 01:45:45 -0400

    working on purchase router

--- routes/PurchaseRouter.js
+++ routes/PurchaseRouter.js
@@ -1,5 +1,37 @@
 const express = require('express')
 const PurchaseRouter = express.Router()
-const { Purchase, Apparal, User, Size } = require('../database/models')
+const { Purchase, Apparel, User, Size } = require('../database/models')
+
+PurchaseRouter.get('/', async (req, res) => {
+	try {
+		const purchases = await Purchase.findAll()
+		res.send(purchases)
+	} catch (error) {
+		throw error
+	}
+})
+
+PurchaseRouter.get('/totals', async (req, res) => {
+	const usdFormatter = new Intl.NumberFormat('en-us', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2
+	})
+	try {
+		const purchases = await Purchase.findAll()
+		const items = await Apparel.findAll()
+		const resp = []
+		for (let i = 0; i < items.length; i++) {
+			// console.log(items[i].dataValues)
+			let item = items[i].dataValues
+			let profit = parseFloat(item.price) - parseFloat(item.cost)
+			let data = { productName: item.name, profit: usdFormatter.format(profit) }
+			resp.push(data)
+		}
+		res.send(resp)
+	} catch (error) {
+		throw error
+	}
+})
 
 module.exports = PurchaseRouter
