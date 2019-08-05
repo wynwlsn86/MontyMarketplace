05063f39ab07ec0198ad2caecdfecfdc2abb9860
Andre Pato <anpato1994@gmail.com>
2019-08-05 13:35:03 -0400

    set up routes and basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Route, Switch, Link } from 'react-router-dom'
+import { Header } from './components/common'
+import Home from './components/Home'
+import About from './components/About'
+import Products from './components/Products'
+import Product from './components/Product'
+import Contact from './components/Contact'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+				<Link to="/marketplace">Products</Link>
+				<Link to="/contact">Contact</Link>
+			</Header>
+			<main>
+				<Switch>
+					<Route exact path="/" render={(props) => <Home {...props} />} />
+					<Route path="/about" render={(props) => <About {...props} />} />
+					<Route
+						path="/marketplace"
+						render={(props) => <Products {...props} />}
+					/>
+					<Route
+						path="/marketplace/:item_id"
+						render={(props) => <Product {...props} />}
+					/>
+					<Route path="/contact" render={(props) => <Contact {...props} />} />
+				</Switch>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ client/src/components/common/Header.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Header = ({ title, children }) => {
+	return <header>{children}</header>
+}
