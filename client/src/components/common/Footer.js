70d41d8425e3b009b2f75f6d3524cadf6198c03b
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-05 17:24:55 -0400

    added footer component and style sheets

---
+++ client/src/components/common/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-container">
+        <footer>
+          <p>COPYRIGHT © 2019 MONTY MARKET</p>
+        </footer>
+      </div>
+    );
+  }
+}
--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,5 +1,60 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+// import axios from "axios";
+import "./Header.css";
 
-export const Header = ({ title, children }) => {
-	return <header>{children}</header>
+import Home from "../../components/Home";
+import About from "../../components/About";
+import Apparel from "../../components/Apparel";
+import Phones from "../../components/Phones";
+import Product from "../Apparel";
+import Contact from "../../components/Contact";
+
+import headerLogo from "../../assets/Monty-logo2.png";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header-container">
+          <div className="header-logo-container">
+            <img src={headerLogo} alt="Monty logo" className="header-logo" />
+          </div>
+          
+            <div className="nav-column-one">
+              <li className="nav-one">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-one">
+                <Link to="/marketplace">Apparel</Link>
+              </li>
+              <li className="nav-one">
+                <Link to="/marketplace">Phones</Link>
+              </li>
+            </div>
+            <div className="nav-column-two">
+              <li className="nav-two">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="nav-two">
+                <Link to="/contact">Contact</Link>
+              </li>
+            </div>
+          
+        </div>
+        <Switch>
+          <Route exact path="/" render={props => <Home {...props} />} />
+          <Route path="/about" render={props => <About {...props} />} />
+          <Route path="/marketplace" render={props => <Apparel {...props} />} />
+          <Route path="/marketplace" render={props => <Phones {...props} />} />
+          <Route
+            path="/marketplace/:item_id"
+            render={props => <Product {...props} />}
+          />
+          <Route path="/contact" render={props => <Contact {...props} />} />
+        </Switch>
+      </div>
+    );
+  }
 }
+
