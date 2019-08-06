f177727389adaafff77fb97a4ebb99feaeb08e68
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-06 01:59:53 -0400

    created styles folder, working on contact styling

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Header.css'
+import '../../styles/Header.css'
 
 import headerLogo from '../../assets/Monty-logo2.png'
 
