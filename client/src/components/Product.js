d99b07b5c028ed88dad8dba524f9a33800298b86
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-10 16:38:03 -0400

    got rid of container element in product file

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,66 +1,65 @@
-import React, { Component } from 'react'
-import { getProduct, getPhone } from '../services/api'
-import { Container, Image } from './common'
+import React, { Component } from "react";
+import { getProduct, getPhone } from "../services/api";
+import { Container, Image } from "./common";
 
 export default class Product extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			products: [],
-			productId: props.location.state.productId,
-			productType: props.match.params.product_type,
-			isLoading: false
-		}
-	}
-	async componentDidMount() {
-		await this.fetchProduct()
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      productId: props.location.state.productId,
+      productType: props.match.params.product_type,
+      isLoading: false
+    };
+  }
+  async componentDidMount() {
+    await this.fetchProduct();
+  }
 
-	fetchProduct = async () => {
-		if (this.state.productType === 'apparel') {
-			const product = await getProduct(this.state.productId)
-			this.setState({ product })
-		} else {
-			const product = await getPhone(this.state.productId)
-			this.setState({ product })
-		}
-	}
+  fetchProduct = async () => {
+    if (this.state.productType === "apparel") {
+      const product = await getProduct(this.state.productId);
+      this.setState({ product });
+    } else {
+      const product = await getPhone(this.state.productId);
+      this.setState({ product });
+    }
+  };
 
-	renderApparel = () => {
-		const { product } = this.state
-		if (product) {
-			return (
-				<Container classname="products">
-					<h3>{product.name}</h3>
-					<p>${product.price}</p>
-					<p>{product.description}</p>
-				</Container>
-			)
-		}
-	}
+  renderApparel = () => {
+    const { product } = this.state;
+    if (product) {
+      return (
+        <div>
+          <h3>{product.name}</h3>
+          <p>${product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      );
+    }
+  };
 
-	renderPhone = () => {
-		console.log(this.state.product)
-		const { product } = this.state
-		if (product) {
-			return (
-				<Container>
-					<h3>{product.brand}</h3>
-					<Image source={product.imageUrl} alt={product.modelNumber} />
-				</Container>
-			)
-		}
-	}
+  renderPhone = () => {
+    console.log(this.state.product);
+    const { product } = this.state;
+    if (product) {
+      return (
+        <div>
+          <h3>{product.brand}</h3>
+          <Image source={product.imageUrl} alt={product.modelNumber} />
+        </div>
+      );
+    }
+  };
 
-	render() {
-		return (
-			<Container classname="product-continer">
-				Product
-				<Container classname="filter-container">Filter</Container>
-				{this.state.productType === 'apparel'
-					? this.renderApparel()
-					: this.renderPhone()}
-			</Container>
-		)
-	}
+  render() {
+    return (
+      <div>
+        Product
+        {this.state.productType === "apparel"
+          ? this.renderApparel()
+          : this.renderPhone()}
+      </div>
+    );
+  }
 }
