c39e86f04db3287e4c04ac0f5dbf4fbef8562541
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-14 12:10:48 -0400

    increased size of legend and styled spacing

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -10,23 +10,39 @@ import phonePhoto from "../assets/phone-photo.png";
 export default class DemoCarousel extends Component {
   render() {
     return (
-        <div className="carousel-center">
+      <div className="carousel-center">
         <div className="carousel-container">
-      <Carousel showStatus={false} showThumbs={false} infiniteLoop={true} autoPlay={true} interval={4000}>
-        <div>
-          <img src={shopPhoto} alt="shop" />
-          <p className="legend">Legend 1</p>
+          <Carousel
+            showStatus={false}
+            showThumbs={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            interval={4000}
+          >
+            <div>
+              <img src={shopPhoto} alt="shop" />
+              <p className="legend">
+                Legend 1
+                <br />
+                Content will go here.
+                <br />
+                Breaks are used for separation of lines.
+                <br />
+                Content will be wrapped in a single paragraph tag.
+                <br />
+                Legend has hover effect, flex wrap and line height.
+              </p>
+            </div>
+            <div>
+              <img src={apparelPhoto} alt="apparel" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src={phonePhoto} alt="phone" />
+              <p className="legend">Legend 3</p>
+            </div>
+          </Carousel>
         </div>
-        <div>
-          <img src={apparelPhoto} alt="apparel" />
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src={phonePhoto} alt="phone" />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
-      </div>
       </div>
     );
   }
