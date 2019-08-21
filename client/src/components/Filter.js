a0781589cc3d5783c43549a8662f74bcc66da712
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-21 11:04:23 -0400

    styled filter and buttons

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,73 +1,53 @@
-import React, { Component } from 'react'
-import { getCategories } from '../services/api'
+import React, { Component } from "react";
 
-import { ExpansionPanel, ExpansionList } from 'react-md'
+import { ExpansionPanel, ExpansionList } from "react-md";
 
-import '../styles/Filter.css'
+import "../styles/Filter.css";
 
 export default class Filter extends Component {
-	constructor() {
-		super()
-		this.state = {
-			categories: null,
-			isLoading: true
-		}
-	}
-	// fetchCategories = async () => {
-	//   try {
-	// const categories = await getCategories();
-	//     console.log(categories);
-	//     this.setState({ categories, isLoading: false });
-	//   } catch (error) {
-	//     throw error;
-	//   }
-	// };
+  constructor() {
+    super();
+    this.state = {
+      categories: null,
+      isLoading: true
+    };
+  }
 
-	renderCategories = () => {
-		const { categories, addToFilter } = this.props
-		if (categories) {
-			// console.log(categories)
-			return categories.map((category) => {
-				return (
-					<ExpansionPanel label={category.group} key={category._id}>
-						{category.attire.map((attire) => {
-							const group = category.group
-							return (
-								<div key={attire} className="filter-checkbox-container">
-									<input
-										type="checkbox"
-										value={attire}
-										onChange={() => addToFilter({ group, attire })}
-									/>
-									<label className="filter-label" style={{ color: '#333' }}>
-										{attire}
-									</label>
-								</div>
-							)
-						})}
-					</ExpansionPanel>
-				)
-			})
-		}
-	}
+  renderCategories = () => {
+    const { categories, addToFilter } = this.props;
+    if (categories) {
+      return categories.map(category => {
+        return (
+          <ExpansionPanel label={category.group} key={category._id}>
+            {category.attire.map(attire => {
+              const group = category.group;
+              return (
+                <div key={attire} className="filter-checkbox-container">
+                  <input
+                    type="checkbox"
+                    value={attire}
+                    onChange={() => addToFilter({ group, attire })}
+                  />
+                  <label className="filter-label" style={{ color: "#333" }}>
+                    {attire}
+                  </label>
+                </div>
+              );
+            })}
+          </ExpansionPanel>
+        );
+      });
+    }
+  };
 
-	// componentDidMount = async () => {
-	// 	this.fetchCategories()
-	// }
-
-	render() {
-		return (
-			<div className="filter-container">
-				<ExpansionList>{this.renderCategories()}</ExpansionList>
-				<button onClick={this.props.renderFilteredProducts}>Apply</button>
-				<button onClick={this.props.fetchProducts}>Clear Filter</button>
-			</div>
-		)
-	}
-}
-
-{
-	/* <div >
-<input type="checkbox" id={attire} name={attire} />
-<label for={attire}> */
+  render() {
+    return (
+      <div className="filter-container">
+        <ExpansionList>{this.renderCategories()}</ExpansionList>
+        <div className="filter-buttons-container">
+        <button className="filter-apply-button" onClick={this.props.renderFilteredProducts}>Apply</button>
+        <button className="filter-clear-button" onClick={this.props.fetchProducts}>Clear Filter</button>
+      </div></div>
+    );
+  }
 }
