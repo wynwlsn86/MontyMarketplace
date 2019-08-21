2a3e59c195f74032acb9c91ef1f33cbe7ee9ae04
Rachel Gonzalez <dancearchitect@gmail.com>
2019-08-21 12:37:29 -0400

    fixed prop in image component

--- client/src/components/common/Image.js
+++ client/src/components/common/Image.js
@@ -2,10 +2,10 @@ import React from "react";
 
 
 
-export const Image = ({ source, alt, classname }) => {
+export const Image = ({ source, alt, className }) => {
   return (
  
-      <img src={source} alt={alt} className={classname} />
+      <img src={source} alt={alt} className={className} />
 
   );
 };
