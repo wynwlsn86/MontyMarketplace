e109c64554c200f81e205c7302a689185119072c
Andre Pato <anpato1994@gmail.com>
2019-12-15 16:30:21 -0500

    added changelog

--- src/database/models/UserModel.js
+++ src/database/models/UserModel.js
@@ -2,25 +2,11 @@ import { Schema } from 'mongoose'
 
 const UserSchema = new Schema(
   {
-    name: {
-      first: {
-        type: String,
-        required: true
-      },
-      last: {
-        type: String,
-        required: true
-      }
-    },
     password_digest: {
       type: String,
       required: true
     },
     email: {
-      type: String,
-      required: true
-    },
-    username: {
       type: String,
       required: true
     }
